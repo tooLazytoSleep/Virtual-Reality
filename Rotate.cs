f6a3fd0f81fb0d909329b9e04b6bbe8d70976f37
Hanlin Tian <34140686+tooLazytoSleep@users.noreply.github.com>
2018-01-27 14:57:12 -0600

    Add files via upload

---
+++ Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public GameObject Axis;//轴,用于选择围绕中心
+
+    public float sRotateSpeed;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(-Vector3.down * sRotateSpeed, Space.World);
+    }
+}
